384919d04a384854306c9f6b6cb1136ad30b0a83
LironBayda <34834883+LironBayda@users.noreply.github.com>
2020-07-20 22:27:34 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150720
+{
+    class Citizen
+    {
+        public string _name;
+         public  Citizen[] _children;
+        public readonly int _fhatherID;
+        public readonly int _ID;
+
+        public static int _numberOfCurrentCitizens=0;
+        public  const int _maximumNumberOfCurrentCitizens=10000000;
+
+        public Citizen(string name, int fatherID)
+        {
+            _name = name;
+            _children = null;
+            _fhatherID = fatherID;
+            _numberOfCurrentCitizens++;
+            if (_numberOfCurrentCitizens == _maximumNumberOfCurrentCitizens)
+                Console.WriteLine("nummber of citizens reach the maximum.you cant make anther one");
+            _ID = 100000000 + _numberOfCurrentCitizens;
+
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"number Of Current Citizens {_numberOfCurrentCitizens}");
+        }
+
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            return _numberOfCurrentCitizens >= _maximumNumberOfCurrentCitizens;
+        }
+
+        public void PrintId()
+        {
+            Console.WriteLine($"ID: {_ID}");
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine($"the gap: {_ID - _fhatherID}"); 
+        }
+
+        public void setChildren(Citizen[] citizens)
+        {
+            if (_children == null)
+            {
+                _children = citizens;
+            }
+            else
+            {
+                Citizen[] temp =new Citizen[_children.Length+citizens.Length];
+                for (int i = 0; i < _children.Length;i++)
+                {
+                    temp[i] = _children[i];
+                }
+
+                for (int i= _children.Length;i<temp.Length;i++)
+                {
+                    temp[i] = citizens[i- _children.Length];
+                }
+
+               
+                _children =temp;
+            }
+
+        }
+        public override string ToString()
+        {
+            return base.ToString()+$"_fhatherID: {_fhatherID} _ID: {_ID} _maximumNumberOfCurrentCitizens {_maximumNumberOfCurrentCitizens} _numberOfCurrentCitizens: {_numberOfCurrentCitizens} _name: {_name}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150720
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen citizen = new Citizen("Gabi", 100000000);
+            Citizen citizenkid1 = new Citizen("Dana", 100000001);
+            Citizen citizenkid2 = new Citizen("mor", 100000001);
+            Citizen citizenkid3 = new Citizen("Dan", 100000001);
+            Citizen[] kids = { citizenkid1, citizenkid2 };
+            citizen.setChildren(kids);
+            Citizen[] kid3 = { citizenkid3 };
+            citizen.setChildren(kid3);
+        
+
+
+
+        }
+
+        private static bool CheckValidity(Citizen citizen)
+        {
+            if (HasChildren(citizen))
+            {
+                for (int i = 0; i < citizen._children.Length; i++)
+                {
+                    Console.WriteLine($"citizen._children[i]._fhatherID:{citizen._children[i]._fhatherID} citizen._ID { citizen._ID}");
+
+                    if (citizen._children[i]._fhatherID != citizen._ID)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool HasChildren(Citizen citizen)
+        {
+            return citizen._children != null;
+        }
+    }
+}
